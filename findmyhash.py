c9095bbe408d85eda0fd15c735c3adc683b1a78d
Quentin POIRIER <adroneus@gmail.com>
2014-05-01 22:58:22 +0200

    removed old user agents causing bugs in google search, remove debug print, and actualy used hashlib.algorithms

--- findmyhash.py
+++ findmyhash.py
@@ -54,31 +54,35 @@ LDAP_SHA1 = "ldap_sha1"
 
 
 USER_AGENTS = [
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; \
-        SV1; Crazy Browser 1.0.5)",
-    "curl/7.7.2 (powerpc-apple-darwin6.0) libcurl 7.7.2 (OpenSSL 0.9.6b)",
-    "Mozilla/5.0 (X11; U; Linux amd64; en-US; rv:5.0) Gecko/20110619 \
-        Firefox/5.0",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b8pre) Gecko/20101213 \
-        Firefox/4.0b8pre",
-    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0) \
-        chromeframe/10.0.648.205",
-    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; \
-        InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; \
-        .NET CLR 2.0.50727)",
-    "Opera/9.80 (Windows NT 6.1; U; sv) Presto/2.7.62 Version/11.01",
-    "Opera/9.80 (Windows NT 6.1; U; pl) Presto/2.7.62 Version/11.00",
-    "Opera/9.80 (X11; Linux i686; U; pl) Presto/2.6.30 Version/10.61",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 \
-        (KHTML, like Gecko) Chrome/15.0.861.0 Safari/535.2",
-    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.2 (KHTML, like Gecko) \
-        Chrome/15.0.872.0 Safari/535.2",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) \
-        Chrome/14.0.812.0 Safari/535.1",
-    "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
 ]
+# USER_AGENTS = [
+#     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; \
+#         SV1; Crazy Browser 1.0.5)",
+#     "curl/7.7.2 (powerpc-apple-darwin6.0) libcurl 7.7.2 (OpenSSL 0.9.6b)",
+#     "Mozilla/5.0 (X11; U; Linux amd64; en-US; rv:5.0) Gecko/20110619 \
+#         Firefox/5.0",
+#     "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b8pre) Gecko/20101213 \
+#         Firefox/4.0b8pre",
+#     "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)",
+#     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)",
+#     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0) \
+#         chromeframe/10.0.648.205",
+#     "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; \
+#         InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; \
+#         .NET CLR 2.0.50727)",
+#     "Opera/9.80 (Windows NT 6.1; U; sv) Presto/2.7.62 Version/11.01",
+#     "Opera/9.80 (Windows NT 6.1; U; pl) Presto/2.7.62 Version/11.00",
+#     "Opera/9.80 (X11; Linux i686; U; pl) Presto/2.6.30 Version/10.61",
+#     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 \
+#         (KHTML, like Gecko) Chrome/15.0.861.0 Safari/535.2",
+#     "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.2 (KHTML, like Gecko) \
+#         Chrome/15.0.872.0 Safari/535.2",
+#     "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) \
+#         Chrome/14.0.812.0 Safari/535.1",
+#     "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+# ]
 
 
 def crackHash(algorithm, hashvalue=None, hashfile=None):
@@ -154,19 +158,9 @@ https://github.com/Talanor/findmyhash\n" % (str(info[0]), str(info[1])))
             if result:
 
                 # If it is a hashlib supported algorithm...
-                if algorithm in [
-                        MD4,
-                        MD5,
-                        SHA1,
-                        SHA224,
-                        SHA384,
-                        SHA256,
-                        SHA512,
-                        RIPEMD
-                        ]:
+                if algorithm in hashlib.algorithms:
                     # Hash value is calculated to compare with cracker result
                     h = hashlib.new(algorithm)
-                    print(type(result), result)
                     h.update(utils.to_bytes(result))
 
                     if h.hexdigest() == activehash:
