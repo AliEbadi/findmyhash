fb16154571144886494d86ed656e38a18e5890b5
Quentin POIRIER <adroneus@gmail.com>
2014-08-18 18:44:27 +0200

    setup.py functionnal?

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import os.path
+
+rdir = os.path.split(os.path.realpath(__file__))[0]
+
+modules = []
+
+for root, dirs, files in os.walk(rdir):
+    if '/.' in root:
+        continue
+    files = [os.path.relpath(os.path.join(root, s[:len(s) - 3]), rdir) for s in files if s.endswith(".py")]
+    modules = modules + files
+
+
+print(modules)
 
 setup(
     name="libfindmyhash",
@@ -9,5 +24,5 @@ setup(
     url="https://github.com/Talanor/findmyhash",
     maintainer="Quentin POIRIER",
     maintainer_email="quentin.poirier@epitech.eu",
-    py_modules=['modules']
+    py_modules=modules
 )
